37cc6928a3ee4d82c908908180bcc0c6c451bc68
Romain Schonholzer <romain.schonholzer@epita.fr>
2023-11-22 11:27:46 +0100

    chiffrement d'un fichier sql avec AES

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+from Crypto import Random
+from Crypto.Cipher import AES
+
+# Vecteurs d'initialisation
+iv_AES = Random.new().read(AES.block_size)
+
+key_AES = 'abcdefghijklmnop'
+
+aese = AES.new(key_AES, AES.MODE_CFB, iv_AES)
+aesd = AES.new(key_AES, AES.MODE_CFB, iv_AES)
+
+
+plaintext = 'Hello! World'
+
+plaintext = aesd.decrypt(aese.encrypt(plaintext))
+
+print(plaintext)
